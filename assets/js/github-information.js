22bf125fb60726e9f74f9f238b04845b29732ac2
randyaajr <67880000+randyaajr@users.noreply.github.com>
2020-10-15 17:08:45 -0400

    Created a function using jQuery to set the HTML inside targeted element

---
+++ assets/js/github-information.js
@@ -0,0 +1,14 @@
+function fetchGitHubInformation(event) {
+
+    var username = $("#gh-username").val();
+    if (!username) {
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+
+        return;
+    }
+
+    $("#gh-user-data").html(
+        `<div id="loader">
+          <img src="assets/css/loading.gif" alt="loading">
+        </div>`);
+}
